a39f9d1b9610d37164fa57c2a10ea6edbec9b71f
冯靖 <sxyx2008@163.com>
2020-02-01 21:49:09 +0800

    Add configurability to the Clock

--- lib/clock_face.dart
+++ lib/clock_face.dart
@@ -1,8 +1,16 @@
+import 'package:clock/clock_text.dart';
+import 'package:flutter/material.dart';
 import 'package:clock/clock_dial_painter.dart';
 import 'package:clock/clock_hands.dart';
-import 'package:flutter/material.dart';
 
-class ClockFace extends StatelessWidget {
+class ClockFace extends StatelessWidget{
+
+  final DateTime dateTime;
+  final ClockText clockText;
+  final bool showHourHandleHeartShape;
+
+  ClockFace({this.clockText = ClockText.arabic, this.showHourHandleHeartShape = false, this.dateTime});
+
   @override
   Widget build(BuildContext context) {
     return new Padding(
@@ -15,17 +23,20 @@ class ClockFace extends StatelessWidget {
             shape: BoxShape.circle,
             color: Colors.white,
           ),
+
           child: new Stack(
             children: <Widget>[
-              //dial and numbers go here
+              //dial and numbers
               new Container(
                 width: double.infinity,
                 height: double.infinity,
                 padding: const EdgeInsets.all(10.0),
-                child: new CustomPaint(
-                  painter: new ClockDialPainter(clockText: ClockText.roman),
+                child:new CustomPaint(
+                  painter: new ClockDialPainter(clockText: clockText),
                 ),
               ),
+
+
               //centerpoint
               new Center(
                 child: new Container(
@@ -37,12 +48,17 @@ class ClockFace extends StatelessWidget {
                   ),
                 ),
               ),
-              //clock hands go here
-              new ClockHands()
+
+
+              new ClockHands(dateTime:dateTime, showHourHandleHeartShape: showHourHandleHeartShape),
+
             ],
           ),
         ),
+
       ),
     );
   }
 }
+
+
--- lib/hand_second.dart
+++ lib/hand_second.dart
@@ -1,44 +1,45 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class SecondHandPainter extends CustomPainter {
+class SecondHandPainter extends CustomPainter{
   final Paint secondHandPaint;
   final Paint secondHandPointsPaint;
 
   int seconds;
 
-  SecondHandPainter({this.seconds})
-      : secondHandPaint = new Paint(),
-        secondHandPointsPaint = new Paint() {
-    secondHandPaint.color = Colors.red;
-    secondHandPaint.style = PaintingStyle.stroke;
-    secondHandPaint.strokeWidth = 2.0;
+  SecondHandPainter({this.seconds}):
+        secondHandPaint= new Paint(),
+        secondHandPointsPaint= new Paint(){
+    secondHandPaint.color= Colors.red;
+    secondHandPaint.style= PaintingStyle.stroke;
+    secondHandPaint.strokeWidth= 2.0;
 
-    secondHandPointsPaint.color = Colors.red;
-    secondHandPointsPaint.style = PaintingStyle.fill;
+    secondHandPointsPaint.color=Colors.red;
+    secondHandPointsPaint.style= PaintingStyle.fill;
+
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    final radius = size.width / 2;
+    final radius= size.width/2;
     canvas.save();
 
     canvas.translate(radius, radius);
 
-    canvas.rotate(2 * pi * this.seconds / 60);
+    canvas.rotate(2*pi*this.seconds/60);
 
-    Path path1 = new Path();
+    Path path1= new Path();
     Path path2 = new Path();
-    path1.moveTo(0.0, -radius);
-    path1.lineTo(0.0, radius / 4);
+    path1.moveTo(0.0, -radius );
+    path1.lineTo(0.0, radius/4);
 
-    path2.addOval(
-        new Rect.fromCircle(radius: 7.0, center: new Offset(0.0, -radius)));
-    path2.addOval(
-        new Rect.fromCircle(radius: 5.0, center: new Offset(0.0, 0.0)));
+    path2.addOval(new Rect.fromCircle(radius: 7.0, center: new Offset(0.0, -radius)));
+    path2.addOval(new Rect.fromCircle(radius: 5.0, center: new Offset(0.0, 0.0)));
 
     canvas.drawPath(path1, secondHandPaint);
     canvas.drawPath(path2, secondHandPointsPaint);
+
+//    canvas.drawShadow(path2, Colors.black, 4.0, false);
 
     canvas.restore();
   }
